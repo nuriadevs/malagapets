92d081783910e4b9a61eeb565d1cd3e4baade563
Nuria <nuriadevs@gmail.com>
2025-11-22 19:18:29 +0100

    fix: remove url config for Strapi Cloud compatibility

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -3,7 +3,7 @@ export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   // The public URL of the Strapi backend
-  url: env("STRAPI_ADMIN_BACKEND_URL"),
+//  url: env("STRAPI_ADMIN_BACKEND_URL"),
   app: {
     keys: env.array("APP_KEYS"),
   },
