0c4ad0cd8084396e7b371100d99fb403b873e170
Nuria <nuriadevs@gmail.com>
2025-11-22 11:43:31 +0100

    fix: Ready for Strapi Cloud deployment

---
+++ backend/config/env/production/database.ts
@@ -0,0 +1,21 @@
+// config/env/production/database.ts
+export default ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", false),
+      },
+    },
+    pool: {
+      min: env.int("DATABASE_POOL_MIN", 2),
+      max: env.int("DATABASE_POOL_MAX", 10),
+    },
+    acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+  },
+});
--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,11 +1,13 @@
 // config/server.ts
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  // The public URL of the Strapi backend
+  url: env("STRAPI_ADMIN_BACKEND_URL"),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  }
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
 });
